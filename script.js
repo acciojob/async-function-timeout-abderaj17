cfb5244af77c75125ade89ae10944b7f4bfce8f3
Adarsh Agrahari <115093974+abderaj17@users.noreply.github.com>
2024-11-13 12:29:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+const inputText = document.querySelector("#text")
+const delayField = document.querySelector("#delay");
+const submitBtn = document.querySelector("#btn");
+const outputField = document.querySelector("#output")
+
+async function displayDelayedOutput() {
+ let display = new Promise((resolve)=>{
+    setTimeout(() => {
+        resolve(inputText.value)
+        
+    }, delayField.value);
+ }) 
+ let result = await display;
+ outputField.innerHTML = result;  
+}
+
+submitBtn.addEventListener('click', displayDelayedOutput);
